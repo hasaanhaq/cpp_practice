21b5df97d0bc0a8852092805743d009f9f6ccf9f
hasaanhaq04 <hhaq@ucsd.edu>
2025-02-25 18:22:04 -0800

    commit6

---
+++ twoPointerReverseString.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void reverseString(vector<char>& s){
+    int left = 0;
+    int right = s.size() - 1;
+
+    while(left < right){
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        left++;
+        right--;
+    }
+    
+}
+
+int main(){
+    string str;
+    cout << "Please enter a string: ";
+    cin >> str;
+    vector<char> s(str.begin(), str.end());
+
+    reverseString(s);
+
+   for(char c : s){
+    cout << c;
+   }
+   cout << endl;
+
+   return 0;
+
+}
